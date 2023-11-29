8aaa53f1f33b5d1a5836ad3c353e1e6ab9a37157
nimajneBG <49725968+nimajneBG@users.noreply.github.com>
2023-11-29 19:13:15 +0100

    Add files via upload

---
+++ sortier-algortithmen/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('alpine:init', () => {
+	Alpine.data('selectionSort', () => ({
+		initial_array: [0],
+		step_count: 0,
+		current_pos: 0, 
+        get finished() { return this.current_pos >= (this.initial_array.length - 1) },
+		step() {
+			let smallest = this.current_pos
+			for (let i = this.current_pos + 1; i < this.initial_array.length; i++) {
+				if (this.initial_array[i] < this.initial_array[smallest]) { smallest = i }
+				this.step_count++
+			}
+			
+			let temp = this.initial_array[smallest]
+			this.initial_array[smallest] = this.initial_array[this.current_pos]
+			this.initial_array[this.current_pos] = temp
+			
+			this.current_pos++
+		},
+        reset() {
+            this.step_count = 0
+            this.current_pos = 0
+            this.initial_array = []
+        },
+        receive(list) {
+            let overwrite = true
+            if (!this.finished) {
+                overwrite = confirm('Daten in Selection-Sort für neues Array zurücksetzen?')
+            }
+
+            if (overwrite) {
+                this.reset()
+                this.initial_array = list
+            }
+        }
+	}))
+
+    Alpine.data('input', () => ({
+		input_text: '',
+        invalid_flag: false,
+		send() {
+            let initial_array = this.input_text.split(',').map(Number)
+            this.$dispatch('newarray', initial_array)
+        }
+	}))
+})
