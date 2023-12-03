b363b4b442e534e4a386a64ece91c4620f463de2
nimajneBG <49725968+nimajneBG@users.noreply.github.com>
2023-12-03 13:34:22 +0100

    Update script.js

--- sortier-algortithmen/script.js
+++ sortier-algortithmen/script.js
@@ -1,26 +1,36 @@
 document.addEventListener('alpine:init', () => {
-	Alpine.data('selectionSort', () => ({
-		initial_array: [0],
-		step_count: 0,
-		current_pos: 0, 
+    Alpine.data('selectionSort', () => ({
+        initial_array: [0],
+        step_count: 0,
+        current_pos: 0,
+        toggle: true,
+        smallest: -1, 
         get finished() { return this.current_pos >= (this.initial_array.length - 1) },
-		step() {
-			let smallest = this.current_pos
-			for (let i = this.current_pos + 1; i < this.initial_array.length; i++) {
-				if (this.initial_array[i] < this.initial_array[smallest]) { smallest = i }
-				this.step_count++
-			}
-			
-			let temp = this.initial_array[smallest]
-			this.initial_array[smallest] = this.initial_array[this.current_pos]
-			this.initial_array[this.current_pos] = temp
-			
-			this.current_pos++
-		},
+        step() {
+            if (this.toggle) {
+                let smallest = this.current_pos
+                for (let i = this.current_pos + 1; i < this.initial_array.length; i++) {
+                    if (this.initial_array[i] < this.initial_array[smallest]) { smallest = i }
+                    this.step_count++
+                }
+                this.smallest = smallest
+                this.toggle = false
+            } else {
+                let temp = this.initial_array[this.smallest]
+                this.initial_array[this.smallest] = this.initial_array[this.current_pos]
+                this.initial_array[this.current_pos] = temp
+                
+                this.smallest = this.current_pos
+                this.current_pos++
+                this.toggle = true
+            }
+        },
         reset() {
             this.step_count = 0
             this.current_pos = 0
             this.initial_array = []
+            this.toggle = true
+            this.smallest = -1
         },
         receive(list) {
             let overwrite = true
@@ -33,14 +43,14 @@ document.addEventListener('alpine:init', () => {
                 this.initial_array = list
             }
         }
-	}))
+    }))
 
     Alpine.data('input', () => ({
-		input_text: '',
+        input_text: '',
         invalid_flag: false,
-		send() {
+        send() {
             let initial_array = this.input_text.split(',').map(Number)
             this.$dispatch('newarray', initial_array)
         }
-	}))
+    }))
 })
